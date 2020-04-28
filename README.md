7bcb2ce004be125bf22ed1e53e67cefd19e00f51
adamberger20 <aberger20@uchicago.edu>
2020-04-28 11:24:55 -0500

    final commit for project 1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Metcalf_Geocoding
-Program to query the Google Maps Geocoding API for bus addresses
+
+This repo contains data on bus burnings in Sao Paulo, Brazil and a script to query the Google Maps API and geocode (i.e. convert to latitude and longitude) those addresses. This repo will be updated as necessary to manage further tasks on this project. It contains two important folders:
+
+1. __data__: Data on the bus burnings and outputs from geocoding.
+
+1. __scripts__: Script used to geocode the addresses for the bus burnings.
+
+Running those scripts requires two packages:
+
+```
+library(tidyverse)
+library(mapsapi)
+```
--- scripts/geocode.R
+++ scripts/geocode.R
@@ -3,8 +3,6 @@
 # load the necessary pacakges
 library(tidyverse)
 library(mapsapi)
-library(hash)
-library(xml)
 
 # load the data and remove empty rows
 buses_import <- read_csv("data/buses.csv") 
@@ -13,9 +11,11 @@ buses <- buses_import[c(1:343), ]
 # get the addresses as a character vector
 addresses <- buses$Local
 
-# geocode the addresses
+# get geocodes from the Google Maps API (requires key)
 codes <- mp_geocode(addresses, region = "br", key = getOption("google_key"))
+
+# extract lat/long points from the API responses
 point <- mp_get_points(codes)
 
-# save the output
-write_csv(point, "data/points.csv")
+# save the output (remove comment to run)
+# write_csv(point, "data/points.csv")
